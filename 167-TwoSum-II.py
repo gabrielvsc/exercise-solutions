9fdf2b4453a03e42b3fb27103c4829692f539b50
gabrielvsc <gabriel.cesario@ccc.ufcg.edu.br>
2024-01-01 17:00:51 -0300

    feat: add 167-TwoSumII solution

---
+++ 167-TwoSum-II.py
@@ -0,0 +1,18 @@
+# 167. Two Sum II - Input Array Is Sorted
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers)-1
+
+        while i < j:
+            _sum = numbers[i] + numbers[j]
+            if _sum == target:
+                return[i+1, j+1]
+
+            if _sum > target:
+                j-=1
+            else:
+                i+=1
+
+        return []
