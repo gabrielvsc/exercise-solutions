4217400281ae0787527dc48c999d19bb0f46f32c
gabrielvsc <gabriel.cesario@ccc.ufcg.edu.br>
2025-04-15 18:12:33 -0300

    feat: add 1944A Codeforces

--- codeforces/2069A-WasThereAnArray.py
+++ codeforces/2069A-WasThereAnArray.py
@@ -1,20 +1,10 @@
 t = int(input())
- 
+    
 for _ in range(t):
-    n, k = map(int, input().split())
- 
-    if n == 1:
-        print(1)
-        continue
- 
-    tb = (n * (n - 1)) // 2
-    z = k // (n - 1)
- 
-    if k >= tb:
-        print(1)
-    elif k >= (n - 1):
-        print(1)
-    elif z == n:
-        print(z)
+    n = int(input())
+    b = input()
+    
+    if '1 0 1' in b:
+        print("NO")
     else:
-        print(n - z)
+        print("YES")
